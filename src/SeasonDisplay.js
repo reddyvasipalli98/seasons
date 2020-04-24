890d6a81643f1a8ecedcffc13eabb2de9c48bebe
Karthikeya Reddy <KReddy@perdoceoed.com>
2020-04-24 09:20:55 -0500

    seasons info added with icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season display!</div>
+const seasonConfig = {
+    summer : {
+        seasonText : "Lets hit the beach",
+        iconText : 'sun'
+    },
+    winter : {
+        seasonText : 'Burr, its cold',
+        iconText : 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {seasonText, iconText} = seasonConfig[season];
+    
+    return(
+        <div>
+           <i className={`${iconText} icon`}/>
+           <h1>{seasonText}</h1>
+           <i className={`${iconText} icon`}/>
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component{
 
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
 
-        this.state = {lat:null, errorMessage : ''};
+    //     this.state = {lat:null, errorMessage : ''};
 
+        
+    // }
+
+    state = {lat:null, errorMessage : ''};
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({lat: position.coords.latitude});
@@ -27,7 +34,7 @@ class App extends React.Component{
 
         if(!this.state.errorMessage && this.state.lat)
         {
-            return <div>Latitude : {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading...</div>
